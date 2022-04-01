38144ae385eae3346b7640d2a0bd99e1dba8157e
TomaszBogunTCD <78962005+TomaszBogunTCD@users.noreply.github.com>
2022-04-01 11:50:35 +0100

    first commit

--- python/shopping_cart.py
+++ python/shopping_cart.py
@@ -4,7 +4,6 @@ from typing import Dict
 from shopping_cart_interface import IShoppingCart
 from pricer import Pricer
 
-
 class ShoppingCart(IShoppingCart):
     """
     Implementation of the shopping tills in our supermarket.
@@ -12,18 +11,51 @@ class ShoppingCart(IShoppingCart):
     def __init__(self, pricer: Pricer):
         self.pricer = pricer
         self._contents: Dict[str,int] = {}
+        self.itemScanOrder = []
+        # retrieves the format for receipt printing from a local file
+        try:
+            f = open("receiptPrintFormat.txt", "r")
+            self.receiptPrintFormat = f.read()
+            f.close()
+        # if the format file is not found, the format for printing is set to the default printing format
+        except FileNotFoundError:
+            self.receiptPrintFormat = "ITEM: PRICE x QUANTITY = TOTAL"
 
-    def add_item(self, item_type: str, number: int):
+    def add_item(self, itemName: str, quantity: int):
+        # checks the itemName to see if there are any reserved capitalised keywords
+        # that are used exclusively for formatting the receipt print functionality.
+        # replaces them with the lowercase version if they are in the name
+        if "TOTAL" in itemName or "ITEM" in itemName or "QUANTITY" in itemName or "PRICE" in itemName:
+            itemName.replace("TOTAL", "total").replace("ITEM", "item").replace("QUANTITY", "quantity").replace("PRICE", "price")
         # adds new item to or update existing item in the shopping cart
-        if item_type not in self._contents:
-            self._contents[item_type] = number
+        # appends the item onto the scan order array if first time scanning such item
+        if itemName not in self._contents:
+            self.itemScanOrder.append(itemName)
+            self._contents[itemName] = quantity
         else:
-            self._contents[item_type] = self._contents[item_type] + number
+            self._contents[itemName] = self._contents[itemName] + quantity
 
+
+    # prints prices in euros instead of euro cents (eg. €1.56) by dividing the price in eurocents by 100, always with 2 digits after the decimal point
     def print_receipt(self):
-        for key, value in self._contents.items():
-            price = self.pricer.get_price(key)
-            print(f"{key} - {value} - {price}")
+        total = 0
+        # prints items in the order the were scanned
+        for itemName in self.itemScanOrder:
+            quantity = self._contents[itemName]
+            price = self.pricer.get_price(itemName)
+            price /= 100
+            total += (price * quantity)
+            self.print_receipt_line(itemName, price, quantity)
+        print("total: " + "{:.2f}".format(total))
+
+    def print_receipt_line(self, itemName, price, quantity):
+        # a line in the receipt is printed by replacing all capitalised reserved keywords by the passed in variables
+        print(self.receiptPrintFormat
+              .replace("ITEM", str(itemName))
+              .replace("PRICE", str("{:.2f}".format(price)))
+              .replace("QUANTITY", str(quantity))
+              .replace("TOTAL", str("{:.2f}".format(price * quantity))))
+
 
 class ShoppingCartCreator(ABC):
     """
--- python/tests.py
+++ python/tests.py
@@ -9,17 +9,61 @@ class ShoppingCartTest(unittest.TestCase):
         sc.add_item("apple", 2)
         with Capturing() as output:
             sc.print_receipt()
-        self.assertEqual("apple - 2 - 100", output[0])
 
-    def test_doesnt_explode_on_mystery_item(self):
+        self.assertEqual(sc.receiptPrintFormat.replace("ITEM", "apple").replace("PRICE", "1.00").replace("QUANTITY", "2").replace("TOTAL", "2.00"), output[0])
+        self.assertEqual("total: 2.00", output[1])
+
+    def test_non_existent_item(self):
         sc = ShoppingCartConcreteCreator().operation()
         sc.add_item("apple", 2)
         sc.add_item("banana", 5)
         sc.add_item("pear", 5)
         with Capturing() as output:
             sc.print_receipt()
-        self.assertEqual("apple - 2 - 100", output[0])
-        self.assertEqual("banana - 5 - 200", output[1])
-        self.assertEqual("pear - 5 - 0", output[2])
+
+        self.assertEqual(sc.receiptPrintFormat.replace("ITEM", "apple").replace("PRICE", "1.00").replace("QUANTITY", "2").replace("TOTAL", "2.00"), output[0])
+        self.assertEqual(sc.receiptPrintFormat.replace("ITEM", "banana").replace("PRICE", "2.00").replace("QUANTITY", "5").replace("TOTAL", "10.00"), output[1])
+        self.assertEqual(sc.receiptPrintFormat.replace("ITEM", "pear").replace("PRICE", "0.00").replace("QUANTITY", "5").replace("TOTAL", "0.00"), output[2])
+        self.assertEqual("total: 12.00", output[3])
+
+    def test_no_items(self):
+        sc = ShoppingCartConcreteCreator().operation()
+        with Capturing() as output:
+            sc.print_receipt()
+        self.assertEqual("total: 0.00", output[0])
+
+    def test_same_item_multiple_scans(self):
+        sc = ShoppingCartConcreteCreator().operation()
+        sc.add_item("apple", 1)
+        sc.add_item("apple", 2)
+        sc.add_item("apple", 3)
+        with Capturing() as output:
+            sc.print_receipt()
+
+        self.assertEqual(sc.receiptPrintFormat.replace("ITEM", "apple").replace("PRICE", "1.00").replace("QUANTITY", "6").replace("TOTAL", "6.00"), output[0])
+        self.assertEqual("total: 6.00", output[1])
+
+    # test if the order of the items printed is the same as the order they were scanned, even if they
+    def test_items_location_shuffled(self):
+        sc = ShoppingCartConcreteCreator().operation()
+        sc.add_item("apple", 1)
+        sc.add_item("banana", 3)
+        sc.add_item("apple", 4)
+        with Capturing() as output:
+            sc.print_receipt()
+
+        self.assertEqual(sc.receiptPrintFormat.replace("ITEM", "apple").replace("PRICE", "1.00").replace("QUANTITY", "5").replace("TOTAL", "5.00"), output[0])
+        self.assertEqual(sc.receiptPrintFormat.replace("ITEM", "banana").replace("PRICE", "2.00").replace("QUANTITY", "3").replace("TOTAL", "6.00"), output[1])
+        self.assertEqual("total: 11.00", output[2])
+
+    # test support for fractional quantities
+    # eg 0.5 watermelon
+    def test_fractional_quantity(self):
+        sc = ShoppingCartConcreteCreator().operation()
+        sc.add_item("apple", 1.5)
+        with Capturing() as output:
+            sc.print_receipt()
+        self.assertEqual(sc.receiptPrintFormat.replace("ITEM", "apple").replace("PRICE", "1.00").replace("QUANTITY", "1.5").replace("TOTAL", "1.50"), output[0])
+
 
 unittest.main(exit=False)
